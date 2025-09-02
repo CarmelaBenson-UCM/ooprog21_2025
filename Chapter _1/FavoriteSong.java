a3c63e050347ea0cc81f16aee5fba27b0493ea64
CarmelaBenson-UCM <carmelabenson33@gmail.com>
2025-09-02 22:02:29 +0800

    Create FavoriteSong.java

---
+++ Chapter _1/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong {
+    
+    public static void main(String[] args) {
+      
+        System.out.println("I ask you how many times will you pick me up");
+        System.out.println("When I keep on letting you down?");
+        System.out.println("And each time I will fall short of your glory");
+        System.out.println("How far will forgiveness abound?");
+    }
+}
