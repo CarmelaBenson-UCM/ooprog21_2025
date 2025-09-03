4ebd52b280290b9899c2df1a5a944e53da8e8732
CarmelaBenson-UCM <carmelabenson33@gmail.com>
2025-09-03 08:20:12 +0800

    Update Comments.java

--- Chapter _1/Comments.java
+++ Chapter _1/Comments.java
@@ -1,19 +1,19 @@
 public class Comments {
-    /**
-     * This is a documentation comment.
-     * It is used to describe classes, methods, or variables.
-     */
+   
     public static void main(String[] args) {
-    
+        
+        /**
+        * This is a documentation comment.
+        * It is used to describe classes, methods, or variables.
+        */
+        
         // This is a single-line comment
         // It is usually used for short explanations or notes.
-        System.out.println("Hello! This program shows the 3 different types of comments:");
 
         /* 
          * This is a multi-line comment.
          * It can span across several lines.
          */
-        System.out.println("Single-line, multi-line, and documentation comments.");
     }
 }
     
