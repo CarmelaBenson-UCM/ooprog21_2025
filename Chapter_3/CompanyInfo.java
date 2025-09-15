b94eb73fabb11745556040c6a06e6e18c82408d7
CarmelaBenson-UCM <carmelabenson33@gmail.com>
2025-09-15 21:00:23 +0800

    Update CompanyInfo.java

--- Chapter_3/CompanyInfo.java
+++ Chapter_3/CompanyInfo.java
@@ -1,15 +1,15 @@
 public class CompanyInfo {
+    
+    public static void main(String[] a) {
 
-   public static void main(String[] args) {
-   
-      System.out.println("Smart Electronics");
-      displayHours();
-   }
-   
-   public static void displayHours() {
-   
-      System.out.println("Monday-Friday: 8 am to 6 pm");
-      System.out.println("Saturday: 8 am to noon");
-      System.out.println("Sunday: closed");
-   }
+        System.out.println("Smart Electronics");
+        displayHours();
+    }
+
+    public static void displayHours() {
+        System.out.println("Monday - Friday     8 am to 6 pm\n"
+                          +"Saturday            8 am to noon\n"
+                          +"Sunday              closed\n"
+        );
+    }
 }
