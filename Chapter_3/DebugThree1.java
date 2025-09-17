129d6f33187f904645fd8a722a5cdf6f72ad9672
CarmelaBenson-UCM <carmelabenson33@gmail.com>
2025-09-17 08:27:35 +0800

    Create DebugThree1.java

---
+++ Chapter_3/DebugThree1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DebugThree1 {
+
+   public static void main(String args[]) {
+   
+      double check1;
+      double check2;
+      
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the amount of your check >> ");
+      check1 = input.nextInt();
+      
+      System.out.print("Enter the amount of your friend's check >> ");
+      check2 = input.nextInt();
+      calcTip(check1);
+      calcTip(check2);
+   }
+      public static void calcTip(double check) {
+      
+         final double RATE = 0.15;
+         double tip = check;
+         double bill = tip / RATE;
+         System.out.println("The tip should be at least $" + bill);
+   }
+}
