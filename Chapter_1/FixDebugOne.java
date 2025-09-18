ed106dc084d9a5de4720827c6fc25c51f4aefc0d
CarmelaBenson-UCM <carmelabenson33@gmail.com>
2025-09-18 17:11:11 +0800

    Update and rename c to FixDebugOne.java

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,20 @@
+public class FixDebugOne { //FixDebug One --> FixDebugOne
+
+/* This program displays a greeting */
+    public static void Main(String[] args) { //Main --> main ; String args --> String[] args
+        System.out.println("Hello World!");   //Systm --> System ; ("Hello Wold!") --> ("Hello Wold!");
+    }
+}  //no '}' --> put '}'
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. Invalid classname, it must not contain spaces
+2. wrong syntax for the method "Main" instead of "main"
+3. worng construction of the parameter "String args" instead of "String[] args"
+4. wrong syntax for the class "Systm" instead of "System"
+5. lack of ';' at the end of the java statement
+6. missing of closing brace '}' at the end of the class FixDebugOne
+*/
+
